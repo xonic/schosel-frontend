ea06dea869aee674c6163f3434831452cd01a8e1
Thomas Nägele <tom@naegele.it>
2018-02-01 14:47:23 +0100

    - add matches tabs

---
+++ src/components/Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <div class="tabs">
+       <ul>
+         <li v-for="tab in tabs" :key="tab.id" :class="{ 'is-active': tab.isActive }">
+           <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+         </li>
+       </ul>
+     </div>
+     <div class="tabs-details">
+       <slot></slot>
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return { tabs: [] }
+  },
+
+  created () {
+    this.tabs = this.$children
+  },
+
+  methods: {
+    selectTab (selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.href === selectedTab.href)
+      })
+    }
+  }
+}
+</script>
--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="matches">
-    <HelloWorld msg="Matches"/>
+    <tabs>
+      <tab name="Played">
+        <h1>Played</h1>
+      </tab>
+
+      <tab name="Now Playing" :selected="true">
+        <h1>Now Playing</h1>
+      </tab>
+
+      <tab name="Upcoming">
+        <h1>Upcoming</h1>
+      </tab>
+    </tabs>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Tabs from '@/components/Tabs.vue'
+import Tab from '@/components/Tab.vue'
 
 export default {
   name: 'matches',
   components: {
-    HelloWorld
+    Tabs,
+    Tab
   }
 }
 </script>

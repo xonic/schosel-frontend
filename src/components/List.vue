5e51e6704c680b2f3119d4e7318058cdd0342ba7
Thomas Nägele <tom@naegele.it>
2018-02-02 10:23:15 +0100

    - add matches lists for played live and upcoming

---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for='item in items' :key='item.id'>
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array
+  }
+}
+</script>

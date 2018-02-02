7bbf53f82cbeed228563742157f20940a1a812e9
Thomas Nägele <tom@naegele.it>
2018-02-02 09:44:29 +0100

    - matches page: only show tab if there are games to be displayed inside tab

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="matches">
-    <tabs>
-      <tab name="Played">
+  <div class='matches'>
+    <tabs v-if='hasUpcomingMatches ? (hasPlayedMatches || hasLiveMatches) : (hasPlayedMatches && hasLiveMatches)'>
+      <tab name='Played' v-if='hasPlayedMatches'>
         <h1>Played</h1>
       </tab>
 
-      <tab name="Now Playing" :selected="true">
+      <tab name='Now Playing' :selected='true' v-if='hasLiveMatches'>
         <h1>Now Playing</h1>
       </tab>
 
-      <tab name="Upcoming">
+      <tab name='Upcoming' v-if='hasUpcomingMatches'>
         <h1>Upcoming</h1>
       </tab>
     </tabs>
@@ -26,6 +26,69 @@ export default {
   components: {
     Tabs,
     Tab
+  },
+  data () {
+    return {
+      hasPlayedMatches: false,
+      hasLiveMatches: false,
+      hasUpcomingMatches: false,
+      matches: [
+        {
+          'url': '/matches/3',
+          'date': '2018-07-23T18:30:00.000Z',
+          'status': 'scheduled',
+          'team1_name': 'South Georgia and the South Sandwich Islands',
+          'team1_iso': 'SGS',
+          'team2_name': 'Burkina Faso',
+          'team2_iso': 'BFA',
+          'stage': 'Group Stage',
+          'odds': ['medium', 'high', 'low']
+        },
+        {
+          'url': '/matches/4',
+          'date': '2018-02-02T18:30:00.000Z',
+          'status': 'live',
+          'result': 'X',
+          'team1_name': 'South Georgia and the South Sandwich Islands',
+          'team1_iso': 'SGS',
+          'team1_goals': 1,
+          'team2_name': 'Burkina Faso',
+          'team2_iso': 'BFA',
+          'team2_goals': 1,
+          'stage': 'Round of 16',
+          'bets': '/matches/4/bets',
+          'odds': [13.3, 23.2, 5.0]
+        },
+        {
+          'url': '/matches/7',
+          'date': '2018-01-23T18:30:00.000Z',
+          'status': 'over',
+          'result': '2',
+          'team1_name': 'South Georgia and the South Sandwich Islands',
+          'team1_iso': 'SGS',
+          'team1_goals': 1,
+          'team2_name': 'Burkina Faso',
+          'team2_iso': 'BFA',
+          'team2_goals': 4,
+          'stage': 'Final',
+          'bets': '/matches/7/bets',
+          'odds': [13.3, 23.2, 5.0]
+        }
+      ]
+    }
+  },
+  created () {
+    for (var match in this.matches) {
+      if (this.matches[match].status === 'over') {
+        this.hasPlayedMatches = true
+      }
+      if (this.matches[match].status === 'live') {
+        this.hasLiveMatches = true
+      }
+      if (this.matches[match].status === 'scheduled') {
+        this.hasUpcomingMatches = true
+      }
+    }
   }
 }
 </script>

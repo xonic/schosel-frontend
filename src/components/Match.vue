0d04468b7a5c46e8d5cc25fc0982efb41e9f8090
Thomas Nägele <tom@naegele.it>
2018-02-09 15:59:28 +0100

    - fetch matches from server

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -4,25 +4,27 @@
       <div>{{ matchDate }}</div>
       <div>{{ team1_name }} — {{ team2_name }}</div>
       <div>{{ team1_goals }}&nbsp;:&nbsp;{{ team2_goals }}</div>
-      <div>Home&nbsp;{{ odds[0] }}&nbsp;|&nbsp;Draw&nbsp;{{ odds[1] }}&nbsp;|&nbsp;Away&nbsp;{{ odds[2] }}</div>
+      <div v-if="odds && odds.length > 0">Home&nbsp;{{ odds[0] }}&nbsp;|&nbsp;Draw&nbsp;{{ odds[1] }}&nbsp;|&nbsp;Away&nbsp;{{ odds[2] }}</div>
     </div>
     <div class="match match--live" v-if="status === 'live'">
       <div>{{ matchDate }}</div>
       <div>{{ team1_name }} — {{ team2_name }}</div>
       <div>{{ team1_goals }}&nbsp;:&nbsp;{{ team2_goals }}</div>
-      <div>Home&nbsp;{{ odds[0] }}&nbsp;|&nbsp;Draw&nbsp;{{ odds[1] }}&nbsp;|&nbsp;Away&nbsp;{{ odds[2] }}</div>
+      <div v-if="odds && odds.length > 0">Home&nbsp;{{ odds[0] }}&nbsp;|&nbsp;Draw&nbsp;{{ odds[1] }}&nbsp;|&nbsp;Away&nbsp;{{ odds[2] }}</div>
     </div>
     <div class="match match--upcoming" v-if="status === 'scheduled'">
       <div>{{ matchDate }}</div>
       <div>{{ team1_name }} — {{ team2_name }}</div>
-      <div>{{ odds[0] }}&nbsp;|&nbsp;{{ odds[1] }}&nbsp;|&nbsp;{{ odds[2] }}</div>
-      <input type="radio" :id="url + '-home'" value="1" v-model="bet">
-      <label :for="url + '-home'">Home&nbsp;{{ odds[0] }}</label>
-      <input type="radio" :id="url + '-draw'" value="X" v-model="bet">
-      <label :for="url + '-draw'">Draw&nbsp;{{ odds[1] }}</label>
-      <input type="radio" :id="url + '-away'" value="2" v-model="bet">
-      <label :for="url + '-away'">Away&nbsp;{{ odds[2] }}</label>
-      <span>Bet: {{ bet }}</span>
+      <div v-if="odds && odds.length > 0">
+        <div>{{ odds[0] }}&nbsp;|&nbsp;{{ odds[1] }}&nbsp;|&nbsp;{{ odds[2] }}</div>
+        <input type="radio" :id="url + '-home'" value="1" v-model="bet">
+        <label :for="url + '-home'">Home&nbsp;{{ odds[0] }}</label>
+        <input type="radio" :id="url + '-draw'" value="X" v-model="bet">
+        <label :for="url + '-draw'">Draw&nbsp;{{ odds[1] }}</label>
+        <input type="radio" :id="url + '-away'" value="2" v-model="bet">
+        <label :for="url + '-away'">Away&nbsp;{{ odds[2] }}</label>
+        <span>Bet: {{ bet }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -35,19 +37,19 @@ export default {
     }
   },
   props: {
-    url: String,
-    date: String,
-    status: String,
-    result: String,
-    team1_name: String,
-    team1_iso: String,
-    team1_goals: Number,
-    team2_name: String,
-    team2_iso: String,
-    team2_goals: Number,
-    stage: String,
-    bets: String,
-    odds: Array
+    url: "",
+    date: "",
+    status: "",
+    result: "",
+    team1_name: "",
+    team1_iso: "",
+    team1_goals: 0,
+    team2_name: "",
+    team2_iso: "",
+    team2_goals: 0,
+    stage: "",
+    bets: "",
+    odds: []
   },
   computed: {
     matchDate: function() {
--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='matches'>
     <tabs v-if='hasUpcomingMatches ? (hasPlayedMatches || hasLiveMatches) : (hasPlayedMatches && hasLiveMatches)'>
-      <tab name='Played' v-if='hasPlayedMatches'>
+      <tab name='Played' v-if='hasPlayedMatches' :selected='true'>
         <h1>Played</h1>
         <ul>
           <li
@@ -12,7 +12,7 @@
         </ul>
       </tab>
 
-      <tab name='Now Playing' :selected='true' v-if='hasLiveMatches'>
+      <tab name='Now Playing' v-if='hasLiveMatches'>
         <h1>Now Playing</h1>
         <ul>
           <li
@@ -42,6 +42,16 @@
         </ul>
       </tab>
     </tabs>
+    <div v-else>
+      <h1>Played</h1>
+      <ul>
+        <li
+          is="match"
+          v-for="playedMatch in playedMatches"
+          v-bind="playedMatch">
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -50,6 +60,7 @@
 import Tabs from '@/components/Tabs.vue'
 import Tab from '@/components/Tab.vue'
 import Match from '@/components/Match.vue'
+import axios from 'axios'
 
 export default {
   name: 'matches',
@@ -66,109 +77,44 @@ export default {
       playedMatches: [],
       liveMatches: [],
       upcomingMatches: [],
-      matches: [
-        {
-          'url': '/matches/3',
-          'date': '2018-07-23T18:30:00.000Z',
-          'status': 'scheduled',
-          'team1_name': 'South Georgia and the South Sandwich Islands',
-          'team1_iso': 'SGS',
-          'team2_name': 'Burkina Faso',
-          'team2_iso': 'BFA',
-          'stage': 'Group Stage',
-          'odds': ['medium', 'high', 'low']
-        },
-        {
-          'url': '/matches/4',
-          'date': '2018-02-02T18:30:00.000Z',
-          'status': 'live',
-          'result': 'X',
-          'team1_name': 'South Georgia and the South Sandwich Islands',
-          'team1_iso': 'SGS',
-          'team1_goals': 1,
-          'team2_name': 'Burkina Faso',
-          'team2_iso': 'BFA',
-          'team2_goals': 1,
-          'stage': 'Round of 16',
-          'bets': '/matches/4/bets',
-          'odds': [13.3, 23.2, 5.0]
-        },
-        {
-          'url': '/matches/7',
-          'date': '2018-01-23T18:30:00.000Z',
-          'status': 'over',
-          'result': '2',
-          'team1_name': 'South Georgia and the South Sandwich Islands',
-          'team1_iso': 'SGS',
-          'team1_goals': 1,
-          'team2_name': 'Burkina Faso',
-          'team2_iso': 'BFA',
-          'team2_goals': 4,
-          'stage': 'Final',
-          'bets': '/matches/7/bets',
-          'odds': [13.3, 23.2, 5.0]
-        },
-        {
-          'url': '/matches/1',
-          'date': '2018-07-23T18:30:00.000Z',
-          'status': 'scheduled',
-          'team1_name': 'Russia',
-          'team1_iso': 'RUS',
-          'team2_name': 'England',
-          'team2_iso': 'ENG',
-          'stage': 'Group Stage',
-          'odds': ['medium', 'high', 'low']
-        },
-        {
-          'url': '/matches/2',
-          'date': '2018-02-02T18:30:00.000Z',
-          'status': 'live',
-          'result': 'X',
-          'team1_name': 'Austria',
-          'team1_iso': 'AUT',
-          'team1_goals': 1,
-          'team2_name': 'France',
-          'team2_iso': 'FRA',
-          'team2_goals': 0,
-          'stage': 'Round of 16',
-          'bets': '/matches/2/bets',
-          'odds': [13.3, 23.2, 5.0]
-        },
-        {
-          'url': '/matches/5',
-          'date': '2018-01-23T18:30:00.000Z',
-          'status': 'over',
-          'result': '2',
-          'team1_name': 'Germany',
-          'team1_iso': 'DE',
-          'team1_goals': 3,
-          'team2_name': 'Spain',
-          'team2_iso': 'ESP',
-          'team2_goals': 3,
-          'stage': 'Final',
-          'bets': '/matches/5/bets',
-          'odds': [13.3, 23.2, 5.0]
+      matches: []
+    }
+  },
+  methods: {
+    acceptMatches(response) {
+      console.log(response);
+      this.matches = response.data
+      this.splitMatchesByStatus()
+    },
+    splitMatchesByStatus() {
+
+      // Check if we have played/live/upcoming matches
+      // and split the matches into separate Arrays
+      for (var match in this.matches) {
+        if (this.matches[match].status === 'over') {
+          this.hasPlayedMatches = true
+          this.playedMatches.push(this.matches[match])
         }
-      ]
+        if (this.matches[match].status === 'live') {
+          this.hasLiveMatches = true
+          this.liveMatches.push(this.matches[match])
+        }
+        if (this.matches[match].status === 'scheduled') {
+          this.hasUpcomingMatches = true
+          this.upcomingMatches.push(this.matches[match])
+        }
+      }
     }
   },
   mounted () {
-    // Check if we have played/live/upcoming matches
-    // and split the matches into separate Arrays
-    for (var match in this.matches) {
-      if (this.matches[match].status === 'over') {
-        this.hasPlayedMatches = true
-        this.playedMatches.push(this.matches[match])
-      }
-      if (this.matches[match].status === 'live') {
-        this.hasLiveMatches = true
-        this.liveMatches.push(this.matches[match])
-      }
-      if (this.matches[match].status === 'scheduled') {
-        this.hasUpcomingMatches = true
-        this.upcomingMatches.push(this.matches[match])
-      }
-    }
+    // Get all matches from server
+    axios.get('https://www.schosel.net/worlds2018/api/v1/matches')
+      .then(response => {
+        this.acceptMatches(response)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
 </script>

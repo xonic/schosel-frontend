7890f85bbf721ae0d80a93a3b767de99e0ddbff5
Thomas Nägele <tom@naegele.it>
2018-03-29 10:50:30 +0200

    implement store for matches (GET)

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -7,16 +8,34 @@ export default new Vuex.Store({
   state: {
     status: {},
     users: {},
-    matches: {},
+    matches: [],
     bets: {},
     teams: {}
   },
-  mutations: {
-    setMatches(state, matches) {
-      state.matches = matches;
+  actions: {
+    LOAD_MATCHES: function ({ commit }) {
+      axios.get('http://localhost:5000/api/v1/matches', {withCredentials: true}).then((response) => {
+        commit('SET_MATCHES', { matches: response.data })
+      }, (err) => {
+        console.log(err)
+      })
     }
   },
-  actions: {
-
+  mutations: {
+    SET_MATCHES: (state, { matches }) => {
+      state.matches = matches
+      console.log("state.matches", state.matches)
+    }
+  },
+  getters: {
+    playedMatches: state => {
+      return state.matches.filter(match => match.status === "over")
+    },
+    liveMatches: state => {
+      return state.matches.filter(match => match.status === "live")
+    },
+    upcomingMatches: state => {
+      return state.matches.filter(match => match.status === "scheduled")
+    }
   }
 })

0940ce85d9340abee083dcd6ea937bfbe36661aa
Thomas Nägele <tom@naegele.it>
2018-05-15 08:06:34 +0200

    - remove tab logic - replace with nested navigation - split matches & stats into subviews

--- src/router.js
+++ src/router.js
@@ -2,25 +2,49 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Match from './views/Match.vue'
 import Matches from './views/Matches.vue'
+import PlayedMatches from './views/PlayedMatches.vue'
+import LiveMatches from './views/LiveMatches.vue'
+import UpcomingMatches from './views/UpcomingMatches.vue'
 import Score from './views/Score.vue'
 import Stats from './views/Stats.vue'
+import PlayerStats from './views/PlayerStats.vue'
+import TeamStats from './views/TeamStats.vue'
+import MatchStats from './views/MatchStats.vue'
 import Help from './views/Help.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  base: "/wm2018/static/",
+  base: "/",
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      redirect: '/matches'
+      redirect: '/matches/live'
     },
     {
       path: '/matches',
       name: 'matches',
-      component: Matches
+      component: Matches,
+      redirect: '/matches/live',
+      children: [
+        {
+          name: 'played',
+          path: 'played',
+          component: PlayedMatches
+        },
+        {
+          name: 'live',
+          path: 'live',
+          component: LiveMatches
+        },
+        {
+          name: 'upcoming',
+          path: 'upcoming',
+          component: UpcomingMatches
+        }
+      ]
     },
     {
       path: '/score',
@@ -30,7 +54,25 @@ export default new Router({
     {
       path: '/stats',
       name: 'stats',
-      component: Stats
+      component: Stats,
+      redirect: '/stats/players',
+      children: [
+        {
+          name: 'players',
+          path: 'players',
+          component: PlayerStats
+        },
+        {
+          name: 'teams',
+          path: 'teams',
+          component: TeamStats
+        },
+        {
+          name: 'match-stats',
+          path: 'matches',
+          component: MatchStats
+        }
+      ]
     },
     {
       path: '/help',

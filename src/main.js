045e0e3454acbcf4e00424f29cf06962c16722c3
Thomas Nägele <tom@naegele.it>
2018-06-08 08:43:56 +0200

    - add basic GA

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import analytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(analytics, {
+  id: 'UA-83223879-1',
+  router,
+  debug: {
+    enabled: false,
+    trace: false,
+    sendHitTask: process.env.NODE_ENV === 'production'
+  }
+})
 
 new Vue({
   router,

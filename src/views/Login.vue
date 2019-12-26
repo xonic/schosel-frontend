d001dabea35a4dc2348545029b524c384be506a4
Thomas Nägele <tom@naegele.it>
2019-12-26 09:45:00 +0100

    - add empty login page

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Login</h1>
+</template>
+
+<script>
+  // @ is an alias to /src
+  // import Grid from '@/components/Grid.vue'
+
+  export default {
+    name: 'login',
+  }
+</script>

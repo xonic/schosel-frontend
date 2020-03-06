d59cf29f73fdde8eb00efd5624daee6a2b49cd22
Thomas Nägele <tom@naegele.it>
2020-03-06 19:10:14 +0100

    - add basic sorting to rankgrid

--- src/components/RankGrid.vue
+++ src/components/RankGrid.vue
@@ -2,13 +2,25 @@
   <table>
     <thead>
       <tr>
-        <th>Rank</th>
-        <th>Name</th>
-        <th>Score</th>
+        <th @click="sort('rank')">
+          Rank
+          <span v-if="sortKey === 'rank' && sortDirection === 'asc'"> &uarr;</span>
+          <span v-if="sortKey === 'rank' && sortDirection === 'des'"> &darr;</span>
+        </th>
+        <th @click="sort('name')">
+          Name
+          <span v-if="sortKey === 'name' && sortDirection === 'asc'"> &uarr;</span>
+          <span v-if="sortKey === 'name' && sortDirection === 'des'"> &darr;</span>
+        </th>
+        <th @click="sort('score')">
+          Score
+          <span v-if="sortKey === 'score' && sortDirection === 'asc'"> &uarr;</span>
+          <span v-if="sortKey === 'score' && sortDirection === 'des'"> &darr;</span>
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="user in data">
+      <tr v-for="user in sortedData">
         <td>
           <router-link :to="{ name: 'user', params: { id: user.id } }">
             {{ user.rank }}
@@ -35,6 +47,58 @@
     name: "rank-grid",
     props: {
       data: Array,
+    },
+    data () {
+      return {
+        sortKey: 'rank',
+        sortDirection: 'asc'
+      }
+    },
+    computed: {
+      sortedData() {
+        return this.data.sort((a, b) => {
+          switch(this.sortKey) {
+            case 'rank':
+              if(this.sortDirection === 'asc') {
+                return a.rank > b.rank
+              }
+              else {
+                return a.rank < b.rank
+              }
+            break
+            case 'name':
+              if(this.sortDirection === 'asc') {
+                return a.name > b.name
+              }
+              else {
+                return a.name < b.name
+              }
+            break
+            case 'score':
+              if(this.sortDirection === 'asc') {
+                return a.score > b.score
+              }
+              else {
+                return a.score < b.score
+              }
+            break
+            default:
+
+            break
+          }
+        })
+      }
+    },
+    methods: {
+      sort(key) {
+        if(this.sortKey === key) {
+          this.sortDirection === 'asc' ? this.sortDirection = 'des' : this.sortDirection = 'asc'
+        }
+        else {
+          this.sortKey = key
+          this.sortDirection = 'asc'
+        }
+      }
     }
   }
 </script>

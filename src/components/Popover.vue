3467cf9f92371cba60409e8e18798101bf7e986f
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:14:24 +0100

    - add flags

--- src/components/Popover.vue
+++ src/components/Popover.vue
@@ -14,67 +14,64 @@
 </template>
 
 <script>
-export default {
-  props: {
-    items: Array
-  },
-  data () {
-    return {
-      show: false
+  export default {
+    props: {
+      items: Array
+    },
+    data () {
+      return {
+        show: false
+      }
     }
   }
-}
 </script>
 
 <style>
 
-.popover {
-  position: relative;
-}
+  .popover {
+    position: relative;
+  }
 
-.popover__list {
-  position: absolute;
-  top: 100%;
-  right: 0;
-  background: white;
-  box-shadow: 0 4px 8px rgba(0,0,0,.2);
-  border-radius: 12px;
-}
+  .popover__list {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    background: white;
+    box-shadow: 0 4px 8px rgba(0,0,0,.2);
+    border-radius: 12px;
+  }
 
-.popover__link {
-  display: block;
-  padding: 12px 24px;
-  color: black;
-}
+  .popover__link {
+    display: block;
+    padding: 12px 24px;
+    color: black;
+  }
 
-.fade-enter {
-  opacity: 0;
-  transform: translateY(-4px);
-}
+  .fade-enter {
+    opacity: 0;
+    transform: translateY(-4px);
+  }
 
-.fade-enter-active {
-  transition: .15s ease-out;
-}
+  .fade-enter-active {
+    transition: .15s ease-out;
+  }
 
-.fade-enter-to {
-  opacity: 1;
-  transform: translateY(0);
-}
+  .fade-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 
-.fade-leave {
-  opacity: 1;
-  transform: translateY(0);
-}
+  .fade-leave {
+    opacity: 1;
+    transform: translateY(0);
+  }
 
-.fade-leave-active {
-  transition: .075s ease-out;
-}
+  .fade-leave-active {
+    transition: .075s ease-out;
+  }
 
-.fade-leave-to {
-  opacity: 0;
-  transform: translateY(-4px);
-}
-
-
-
+  .fade-leave-to {
+    opacity: 0;
+    transform: translateY(-4px);
+  }
 </style>

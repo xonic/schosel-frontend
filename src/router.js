7bebe8358b150740dfdd30a5e7c9c7a2721a7a13
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:09:03 +0100

    - add matches page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Matches from './views/Matches.vue'
 import About from './views/About.vue'
 
 Vue.use(Router)
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/matches',
+      name: 'matches',
+      component: Matches
     }
   ]
 })
---
+++ src/views/Matches.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="matches">
+    <HelloWorld msg="Matches"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'matches',
+  components: {
+    HelloWorld
+  }
+}
+</script>

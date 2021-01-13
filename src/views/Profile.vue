695dd18f1b76d54c8551324b48cfe18c6de5e2dc
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:14:22 +0100

    - begin implementation of profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,89 @@
 <template>
   <div id="profile">
     <h1>Profile</h1>
+    <div class="profile-form">
+      <form @submit.prevent="onSubmit">
+        <div class="input">
+          <label for="firstName">First Name</label>
+          <input
+                  type="text"
+                  id="firstName"
+                  v-model="firstName">
+        </div>
+        <div class="input">
+          <label for="lastName">Last Name</label>
+          <input
+                  type="text"
+                  id="lastName"
+                  v-model="lastName">
+        </div>
+        <div class="input">
+          <label for="email">Email</label>
+          <input
+                  type="email"
+                  id="email"
+                  v-model="email">
+        </div>
+        <div class="submit">
+          <button type="submit">Save</button>
+          <p v-if="error">{{ error }}</p>
+        </div>
+        <img :src="avatar" />
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
 
   export default {
-    name: 'profile'
+    name: 'profile',
+    data () {
+      return {
+        error: null,
+        email: ''
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'loggedInUser',
+        'avatarUrl',
+        'allUsers'
+      ]),
+      avatar () {
+        if(!this.loggedInUser.name.length) return
+        return this.avatarUrl + this.loggedInUser.firstName + " " + this.loggedInUser.lastName
+      },
+      firstName () {
+        return this.loggedInUser.name.split(' ')[0]
+      },
+      lastName () {
+        return this.loggedInUser.name.split(' ')[1]
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this.email = this.allUsers.find(user => user.user_id === this.loggedInUser.user_id).email
+      }, 300)
+    },
+    methods: {
+      onSubmit () {
+        console.log(this.allUsers)
+
+        // if(!this.firstName || !this.lastName || !this.email || !this.password) {
+        //   this.error = 'Please fill all fields.'
+        //   return
+        // }
+        //
+        // const formData = {
+        //   firstName: this.firstName,
+        //   lastName: this.lastName,
+        //   email: this.email,
+        //   password: this.password,
+        // }
+        // this.$store.dispatch('REGISTER', formData)
+      }
+    }
   }
 </script>

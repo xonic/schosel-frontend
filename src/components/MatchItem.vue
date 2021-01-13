63fc156eac40c6dce7708c94a6c28884092b3a8d
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:11:21 +0100

    - style bet form

--- src/components/MatchItem.vue
+++ src/components/MatchItem.vue
@@ -31,7 +31,7 @@
           </div>
         </div>
       </div>
-      <div class="match-item match-item--live" v-if="status === 'live'">
+      <!-- <div class="match-item match-item--live" v-if="status === 'live'">
         <div class="match-item__teams">{{ team1_name }} — {{ team2_name }}</div>
         <div class="match-item__goals">{{ team1_goals }}&nbsp;:&nbsp;{{ team2_goals }}</div>
         <div class="match-item__time">{{ matchTime }} h</div>
@@ -42,21 +42,32 @@
             <svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M8 12l-4.702 2.472.898-5.236L.392 5.528l5.257-.764L8 0l2.351 4.764 5.257.764-3.804 3.708.898 5.236z" fill="#F8E71C" stroke="#E4D40D" fill-rule="evenodd"/></svg>
           </span>
         </span>
-      </div>
+      </div> -->
     </router-link>
-    <div class="match-item match-item<--upcoming" v-if="status === 'scheduled'">
+    <div class="match-item match-item--upcoming" v-if="status === 'scheduled'">
       <div class="match-item__teams">{{ team1_name }} — {{ team2_name }}</div>
       <div class="match-item__time">{{ matchTime }} h</div>
       <div v-if="odds && ownBet" class="match-item__bet">
-        <div class="match-item__odds">{{ odds["1"].toFixed(2) }}&nbsp;|&nbsp;{{ odds["X"].toFixed(2) }}&nbsp;|&nbsp;{{ odds["2"].toFixed(2) }}</div>
-        <input type="radio" :id="match_id + '-home'" value="1" v-model="ownBet.outcome" @change="postBet()">
-        <label :for="match_id + '-home'">Home&nbsp;{{ odds["1"].toFixed(2) }}</label>
-        <input type="radio" :id="match_id + '-draw'" value="X" v-model="ownBet.outcome" @change="postBet()">
-        <label :for="match_id + '-draw'">Draw&nbsp;{{ odds["X"].toFixed(2) }}</label>
-        <input type="radio" :id="match_id + '-away'" value="2" v-model="ownBet.outcome" @change="postBet()">
-        <label :for="match_id + '-away'">Away&nbsp;{{ odds["2"].toFixed(2) }}</label>
-        <input type="checkbox" :id="match_id + '-supertip'" v-model="ownBet.supertip" @change="postBet()">
-        <label :for="match_id + '-supertip'">Supertip</label>
+        <input class="match__input" type="radio" :id="match_id + '-home'" value="1" v-model="ownBet.outcome" @change="postBet()">
+        <label class="match__label" :for="match_id + '-home'">Home&nbsp;
+          <div class="odds-estimation" v-bind:class="[odds['1']]">
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M9 8v4H3V8H0l6-8 6 8H9z" fill="#5FB100" fill-rule="evenodd"/></svg>
+          </div>
+        </label>
+        <input class="match__input" type="radio" :id="match_id + '-draw'" value="X" v-model="ownBet.outcome" @change="postBet()">
+        <label class="match__label" :for="match_id + '-draw'">Draw&nbsp;
+          <div class="odds-estimation" v-bind:class="[odds['X']]">
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M9 8v4H3V8H0l6-8 6 8H9z" fill="#5FB100" fill-rule="evenodd"/></svg>
+          </div>
+        </label>
+        <input class="match__input" type="radio" :id="match_id + '-away'" value="2" v-model="ownBet.outcome" @change="postBet()">
+        <label class="match__label" :for="match_id + '-away'">Away&nbsp;
+          <div class="odds-estimation" v-bind:class="[odds['2']]">
+            <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M9 8v4H3V8H0l6-8 6 8H9z" fill="#5FB100" fill-rule="evenodd"/></svg>
+          </div>
+        </label>
+        <input class="match__input" type="checkbox" :id="match_id + '-supertip'" v-model="ownBet.supertip" @change="postBet()">
+        <label class="match__label" :for="match_id + '-supertip'">Supertip</label>
       </div>
     </div>
   </div>

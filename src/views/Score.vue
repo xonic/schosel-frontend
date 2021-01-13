3e31ff3d8d3f5b0108632734b08f51cd6ab3a228
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:09:10 +0100

    - add match dummy data - add score dummy data

--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,17 +1,63 @@
 <template>
   <div class="score">
-    <HelloWorld msg="Score"/>
+    <h1>Score</h1>
+    <div>271.43 pts</div>
+    <div>2. place</div>
+    <div class="">
+      <h2>Achievements</h2>
+      <ul>
+        <li>
+          <div class="">
+            National expert
+          </div>
+          <div class="">
+            Most correct bets for one team
+          </div>
+          <div class="">
+            12 times North Korea
+          </div>
+        </li>
+        <li>
+          <div class="">
+            Gambler
+          </div>
+          <div class="">
+            Most bets on highest odds
+          </div>
+          <div class="">
+            18 times
+          </div>
+        </li>
+      </ul>
+      <h2>Ranking</h2>
+      <grid
+        :data="gridData"
+        :columns="gridColumns">
+      </grid>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Grid from '@/components/Grid.vue'
 
 export default {
   name: 'score',
   components: {
-    HelloWorld
+    Grid
+  },
+  data: function () {
+    return {
+      searchQuery: '',
+      gridColumns: ['rank', 'player', 'difference', 'score'],
+      gridData: [
+        { rank: 1, player: 'Chuck Norris', difference: '+28.57', score: '300.00' },
+        { rank: 2, player: 'Bruce Lee (You)', difference: '-', score: '271.43' },
+        { rank: 3, player: 'Jackie Chan', difference: '-21.43', score: '250.00' },
+        { rank: 4, player: 'Jet Li', difference: '-31.43', score: '240.00' }
+      ]
+    }
   }
 }
 </script>

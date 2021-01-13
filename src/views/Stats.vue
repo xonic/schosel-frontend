1cc3b98bd59d37c3d917cd89b5a8cb9c9c22c737
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:09:08 +0100

    - add stats gamblers grid

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -2,7 +2,12 @@
   <div class="stats">
     <tabs>
       <tab name="Players" :selected="true">
-        <h1>Players</h1>
+        <h1>Gamblers</h1>
+        <h2>Most bets on highest odds</h2>
+        <grid
+          :data="gridData"
+          :columns="gridColumns">
+        </grid>
       </tab>
 
       <tab name="Teams">
@@ -18,14 +23,28 @@
 
 <script>
 // @ is an alias to /src
+import Grid from '@/components/Grid.vue'
 import Tabs from '@/components/Tabs.vue'
 import Tab from '@/components/Tab.vue'
 
 export default {
   name: 'stats',
   components: {
+    Grid,
     Tabs,
     Tab
+  },
+  data: function () {
+    return {
+      searchQuery: '',
+      gridColumns: ['name', 'bets'],
+      gridData: [
+        { name: 'Chuck Norris', bets: '18' },
+        { name: 'Bruce Lee', bets: 15 },
+        { name: 'Jackie Chan', bets: 10 },
+        { name: 'Jet Li', bets: 4 }
+      ]
+    }
   }
 }
 </script>

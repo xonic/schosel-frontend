b97e8c2bf61e8dc09eba76c4fb7e81bc738002e6
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:14:13 +0100

    - dont load users

--- src/App.vue
+++ src/App.vue
@@ -90,7 +90,7 @@
       this.$store.dispatch('LOAD_MATCHES')
       this.$store.dispatch('LOAD_STATUS')
       this.$store.dispatch('LOAD_OWN_BETS')
-      this.$store.dispatch('LOAD_USERS')
+      // this.$store.dispatch('LOAD_USERS')
 
       // setInterval( () => {
       //   this.$store.dispatch('LOAD_MATCHES')

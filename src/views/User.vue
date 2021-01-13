f9d65881cea2402ab223bb40a0fd6b74ab21a749
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:13:04 +0100

    - begin user detail page implementation

---
+++ src/views/User.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="wrapper">
+      <clip-loader :loading="loading" :color="color" :size="size"></clip-loader>
+      <div v-if='!loading' class="tabs-details">
+        <transition name="hero" appear>
+          <div>
+            <h1 class="hero__heading">{{ user.name }}</h1>
+            <div class="hero__info">{{ user.score }} pts, ranked {{ user.rank }}.</div>
+          </div>
+        </transition>
+        <transition name="content" appear>
+          <div class="list__items">
+            <grid
+              :data="gridData"
+              :columns="gridColumns">
+            </grid>
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Grid from '@/components/Grid.vue'
+import { HTTP } from '../http-constants'
+import { mapGetters } from 'vuex'
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+
+export default {
+  name: 'match',
+  components: {
+    Grid,
+    ClipLoader
+  },
+  data () {
+    return {
+      user: {},
+      interval: null,
+      gridColumns: ['player', 'bet', 'score'],
+      gridData: [],
+      loading: true,
+      size: "32px",
+      color: "#3EABDC"
+    }
+  },
+  props: ['id'],
+  mounted () {
+    this.loadUserData()
+  },
+  computed: {
+    matchDate: function() {
+      return new Date(this.match.date).toLocaleString()
+    }
+  },
+  methods: {
+    loadUserData: function() {
+
+      HTTP.get('/users/' + this.id, {withCredentials: true}).then((response) => {
+        this.user = response.data
+        // this.setGrid(response.data.bets)
+        this.loading = false
+
+        // if(this.match.status === 'live') {
+        //   this.setLoadingInterval()
+        // }
+      }, (err) => {
+        console.log(err)
+      })
+    },
+    setLoadingInterval: function() {
+      if(!this.interval)
+      {
+        this.interval = setInterval( () => {
+          this.loadMatchData()
+        }, 10000);
+      }
+    },
+    setGrid: function(rawGridData) {
+      this.gridData = []
+      rawGridData.forEach(function(el, i){
+
+        // Calculate score for each player
+        var currentScore = this.match.outcome === el.outcome ? el.points : 0.00
+
+        // Set grid data
+        this.gridData.push({
+          player: el.user.name,
+          bet: el.outcome || "-",
+          score: currentScore.toFixed(2)
+        })
+
+        // Sort grid by score
+        this.gridData.sort(function(obj1, obj2){
+          return obj2.score - obj1.score
+        })
+      }, this)
+    }
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
+  }
+}
+</script>

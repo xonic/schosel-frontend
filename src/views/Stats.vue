1bd39e03ce28a099da0a63b666e3c9d669eb5a4e
Thomas Nägele <xonic@users.noreply.github.com>
2021-01-13 11:09:08 +0100

    - change page title - add stats tas

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="stats">
-    <HelloWorld msg="Stats"/>
+    <tabs>
+      <tab name="Players" :selected="true">
+        <h1>Players</h1>
+      </tab>
+
+      <tab name="Teams">
+        <h1>Teams</h1>
+      </tab>
+
+      <tab name="Matches">
+        <h1>Matches</h1>
+      </tab>
+    </tabs>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Tabs from '@/components/Tabs.vue'
+import Tab from '@/components/Tab.vue'
 
 export default {
   name: 'stats',
   components: {
-    HelloWorld
+    Tabs,
+    Tab
   }
 }
 </script>

8521fe630bd6ee43947c8a1e09c84556ccdf29af
Tom Nägele <xonic@users.noreply.github.com>
2021-01-14 22:20:55 +0100

    - add dummy structure to users page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,6 +1,62 @@
 <template>
-  <h1>Users</h1>
+  <main>
+    <h1>Users</h1>
+    <ul>
+      <li>
+        <router-link :to="{ name: 'user', params: { id: '1' } }">
+          <div>
+            Georg N.
+          </div>
+          <ul>
+            <li>Icon KngGm 1.</li>
+            <li>Icon Oldfsnd 1.</li>
+            <li>Icon Undg 1.</li>
+            <li>Icon Blncd 1.</li>
+            <li>Icon Scrt 1.</li>
+          </ul>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'user', params: { id: '3' } }">
+          <div>
+            Tom N.
+          </div>
+          <ul>
+            <li>Icon KngGm 1.</li>
+            <li>Icon Oldfsnd 1.</li>
+            <li>Icon Undg 1.</li>
+            <li>Icon Blncd 1.</li>
+            <li>Icon Scrt 1.</li>
+          </ul>
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'user', params: { id: '4' } }">
+          <div>
+            Carloz S.
+          </div>
+          <ul>
+            <li>Icon KngGm 1.</li>
+            <li>Icon Oldfsnd 1.</li>
+            <li>Icon Undg 1.</li>
+            <li>Icon Blncd 1.</li>
+            <li>Icon Scrt 1.</li>
+          </ul>
+        </router-link>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'users',
+  computed: {
+    ...mapGetters([
+      'allUsers',
+    ])
+  },
+}
 </script>

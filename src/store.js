2b29264d8de0a479b3c7b284b512d8143cd031a6
Tom Nägele <133157+xonic@users.noreply.github.com>
2026-04-30 08:31:56 +0200

    - replace avatar API

--- src/store.js
+++ src/store.js
@@ -28,7 +28,7 @@ export default new Vuex.Store({
       matches: true,
       users: true
     },
-    avatarUrl: 'https://schosel.net/adorables/400/',
+    avatarUrl: 'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=',
     iconPaths: [
       "i--1.svg",
       "i--2.svg",

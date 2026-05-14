95888d0ac0759c190afefc145920cbc4f556c49b
Tom Nägele <133157+xonic@users.noreply.github.com>
2026-05-14 16:04:02 +0200

    - remove register link from login page - remove debugging info from login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,10 +5,6 @@
         Login
       </h1>
       <div class="to-register">
-        No account yet?
-        <router-link :to="{ name: 'register' }">Register now!</router-link>
-      </div>
-      <div class="to-register">
         Need a new password?
         <router-link :to="{ name: 'requestReset' }">Reset password</router-link>
       </div>
@@ -28,11 +24,6 @@
       <message v-if="this.$route.query.reset" type="success">
         Password updated! You can login now.
       </message>
-      <!-- Remove login-debug div -->
-      <div v-if="dbUri" class="login-debug">
-        <div class="text--small text--gray-20">Current SQLALCHEMY_DATABASE_URI:</div>
-        <pre>{{ dbFilename }}</pre>
-      </div>
       <div class="login-form">
         <form @submit.prevent="onSubmit">
           <div class="input">
@@ -63,17 +54,13 @@
 <script>
   import { mapGetters } from 'vuex'
   import Message from '@/components/Message.vue'
-  // Remove unused imports
-  import { HTTP } from '@/http-constants'
 
   export default {
     name: 'login',
     data () {
       return {
         email: '',
-        password: '',
-        // Remove dbUri from data, as it's only used for debugging and should not be part of the component's state
-        dbUri: null
+        password: ''
       }
     },
     components: {
@@ -82,29 +69,9 @@
     computed: {
       ...mapGetters([
         'errors'
-      ]),
-      // Remove dbFilename computed property, as it's only used for debugging and should not be part of the component's logic
-      dbFilename () {
-        if (!this.dbUri) return null
-        const segments = this.dbUri.split('/')
-        return segments[segments.length - 1] || this.dbUri
-      }
-    },
-    created () {
-      // Remove loadDbUri call from created hook, as it's only used for debugging and should not be part of the component's lifecycle
-      this.loadDbUri()
+      ])
     },
     methods: {
-      // Remove loadDbUri method, as it's only used for debugging and should not be part of the component's methods
-      async loadDbUri () {
-        try {
-          const response = await HTTP.get('/login-info')
-          this.dbUri = response.data.sqlalchemy_database_uri
-        }
-        catch (error) {
-          console.error('Unable to load login debug info:', error)
-        }
-      },
       onSubmit () {
         const formData = {
           email: this.email,

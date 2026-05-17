6964f3baa6676c2e05f5a27769672cd37e65f95a
Tom Nägele <133157+xonic@users.noreply.github.com>
2026-05-17 11:30:18 +0200

    - fix pot calculation on scores page

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -2,8 +2,9 @@
   <main>
     <div class="wrapper">
       <h1 class="h2 main__title">Scores</h1>
-      <div v-if="allUsers && allUsers.length" class="text--center text--small text--gray-20">Total pot: {{ (allUsers.length * 10).toFixed(2) }} &euro;</div>
-      <div v-if="allUsers && allUsers.length" class="main__title text--small text--gray-20">Pot per score: {{ (allUsers.length * 10 / 5).toFixed(2) }} &euro;</div>
+      <div v-if="allUsers && allUsers.length" class="text--center text--small text--gray-20">Total pot: {{ (allUsers.length * 15).toFixed(2) }} &euro;</div>
+      <div v-if="allUsers && allUsers.length" class="text--center text--small text--gray-20">Schosel: {{ (allUsers.length * 15 * 0.7).toFixed(2) }} &euro;</div>
+      <div v-if="allUsers && allUsers.length" class="text--center text--small text--gray-20">Schosel: {{ (allUsers.length * 15 * 0.3).toFixed(2) }} &euro;</div>
       <ul v-if="scorePreviews.length">
         <li v-for="(score, index) in scorePreviews">
           <score-preview :score="scorePreviews[index]" :challenge-id="index" />
